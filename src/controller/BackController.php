4d6a33cac457d7b912fc557f09a4d8ff46e38740
Celine <celi.caurier@gmail.com>
2019-10-14 15:38:37 +0200

    Finish Form Reset Password

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -1,44 +1,71 @@
 <?php
 
-
-
 namespace Project\controller;
+
 use Twig\Environment;
 use Project\model;
-session_start();
+
+
+
 class BackController
 {
     protected $twig = null;
 
-    public function __construct(Environment $twig){
+    public function __construct(Environment $twig)
+    {
         $this->twig = $twig;
     }
 
-    function watch_question(){
+    public function watch_question()
+    {
         if (isset($_POST['username'])) {
-            $BackManager = new model\BackManager();
-            $BackManager->userQuestion($_POST['username']);
-            if (isset($_SESSION['question']) && isset($_SESSION['reponse'])) {
-                echo $this->twig->render('mdpForget.twig', ['user' => [
-                    'username' => $_POST['username'],
-                    'question' => $_SESSION['question'],
-                    'reponse' => $_SESSION['reponse']
-                ]]);
+            $_SESSION = [];
 
+            $_SESSION['username'] = $_POST['username'];
+
+            $BackManager = new model\BackManager();
+            $question = $BackManager->userQuestion($_POST['username']);
+
+            if (isset($question)) {
+
+                $_SESSION['question'] = $question;
+
+                return $this->twig->render('mdpForget.twig', array(
+                    'session' => $_SESSION
+                ));
             } else {
                 echo 'merci de remplir votre Username';
             }
         }
     }
 
-    function control_question(){
+    public function control_question()
+    {
+        $BackManager = new model\BackManager();
+        $answer = $BackManager->userReponse($_SESSION['username']);
 
-          if($_POST['answer']== $_SESSION['reponse']){
-              echo 'ok';
-          }else{
-              echo ' pas ok';
-          }
+        if (isset($answer)) {
+            if ($_POST['answer'] == $answer) {
+
+                return $this->twig->render('mdpForget.twig', array(
+                    'session' => $_SESSION,
+                    'answer' => 'ok'
+                ));
+            } else {
+                return $this->twig->render('mdpForget.twig', array(
+                    'session' => $_SESSION,
+                    'answer' => 'false'
+                ));
+            }
+        }
     }
+
+    public function reset_mdp(){
+        $BackManager = new model\BackManager();
+        $BackManager->resetPassword($_SESSION['username'],$_POST['user_password']);
+
+    }
+
 
 
 
--- src/model/BackManager.php
+++ src/model/BackManager.php
@@ -10,12 +10,28 @@ class BackManager extends Manager
     public function userQuestion($username)
     {
         $bdd = $this->dbConnect();
-        $reqQuestion = $bdd->QUERY("SELECT question, reponse FROM userbank WHERE username='$username'");
+        $reqQuestion = $bdd->QUERY('SELECT question FROM userbank WHERE username="'.$username.'"');
         $data = $reqQuestion->fetch();
 
-        $_SESSION['question'] = $data['question'];
-        $_SESSION['reponse'] = $data['reponse'];
+        $question=$data['question'];
+        return $question;
 
     }
 
+  public function userReponse($username)
+    {
+        $bdd = $this->dbConnect();
+        $reqAnswer = $bdd->QUERY('SELECT reponse FROM userbank WHERE username="'.$username.'"');
+        $data = $reqAnswer->fetch();
+
+        $answer = $data['reponse'];
+        return $answer;
+
+   }
+
+   public function resetPassword($username,$password)
+   {
+       $bdd = $this->dbConnect();
+       $bdd->exec('UPDATE userbank SET password = "'.$password.'" WHERE username = "'.$username.'"');
+   }
 }
