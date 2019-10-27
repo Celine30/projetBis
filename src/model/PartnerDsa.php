66310063b130afa657565001654700e095466167
Celine <celi.caurier@gmail.com>
2019-10-27 19:57:16 +0100

    mistake

--- src/model/PartnerDsa.php
+++ src/model/PartnerDsa.php
@@ -12,7 +12,7 @@ class PartnerDsa extends Partner
     public $resume = 'accélère la croissance du territoire et s’engage avec les collectivités territoriales.
 Nous accompagnons les entreprises dans les étapes clés de leur évolution.
 Notre philosophie : s’adapter à chaque entreprise.
-Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises';
+Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.';
     public $lien = 'contact@dsa.com';
 }
 
