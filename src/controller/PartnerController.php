31e9ac0e1a6a8a9f75ef716d5df58ab23b8ba17b
Celine <celi.caurier@gmail.com>
2019-11-07 13:30:55 +0100

    table Partner

--- src/controller/PartnerController.php
+++ src/controller/PartnerController.php
@@ -3,10 +3,6 @@
 
 namespace Project\Controller;
 
-use Tracy\Debugger;
-Debugger::enable();
-use Project\Model;
-
 class PartnerController extends Controller
 {
      public function actor($message="")
@@ -29,105 +25,5 @@ class PartnerController extends Controller
     {
         $this->view_add_com($_GET['partner']);
     }
-
-
-
-
-
-
-
-
- //   public function partner_cde($message="")
-//    {
-//        $partner = new Model\PartnerCde();
-//        $this->partner('partner_cde',$partner ,$message);
- //   }
-
- //       public function partner_cde_com()
- //   {
- //       $partner = new Model\PartnerCde();
- //       $this->view_add_com($partner);
- //   }
-
-  //   public function partner_cde_good()
-  //  {
-  //      $this->User_avis('partner_cde' , $_SESSION['username'], 'good');
- //   }
-
- //   public function partner_cde_bad()
- //   {
- //       $this->User_avis('partner_cde' , $_SESSION['username'], 'bad');
- //   }
-
-//    public function partner_dsa($message="")
-//    {
-//        $partner = new Model\PartnerDsa();
-//        $this->partner('partner_dsa',$partner ,$message);
-//    }
-
-//    public function partner_dsa_com()
-//    {
-//        $partner = new Model\PartnerDsa();
-//        $this->view_add_com($partner );
- //   }
-
-//     public function partner_dsa_good()
- //   {
- //       $this->User_avis('partner_dsa' , $_SESSION['username'], 'good');
- //   }
-
-//    public function partner_dsa_bad()
- //   {
- //       $this->User_avis('partner_dsa' , $_SESSION['username'], 'bad');
- //   }
-
-//    public function partner_co($message="")
- //   {
- //       $partner = new Model\PartnerCo();
- //       $this->partner('partner_co',$partner ,$message);
-
- //   }
-
- //   public function partner_co_com()
- //   {
- //       $partner = new Model\PartnerCo();
- //       $this->view_add_com($partner );
-
-//    }
-
- //    public function partner_co_good()
- //   {
- //       $this->User_avis('partner_co' , $_SESSION['username'], 'good');
- //   }
-
- //   public function partner_co_bad()
- //   {
- //       $this->User_avis('partner_co' , $_SESSION['username'], 'bad');
- //   }
-
-//    public function partner_pro($message="")
-//    {
-//        $partner = new Model\PartnerPro();
-//        $this->partner('partner_pro',$partner ,$message);
-//    }
-
- //       public function partner_pro_com()
-//    {
-//        $partner = new Model\PartnerPro();
- //       $this->view_add_com($partner );
- //   }
-
- //    public function partner_pro_good()
- //   {
- //       $this->User_avis('partner_pro' , $_SESSION['username'], 'good');
- //   }
-
-//    public function partner_pro_bad()
-//    {
-//        $this->User_avis('partner_pro' , $_SESSION['username'], 'bad');
-//    }
-
-
-
 
 }
--- src/model/BackManager.php
+++ src/model/BackManager.php
@@ -15,7 +15,6 @@ class BackManager extends Manager
 
         $question = $data['question'];
         return $question;
-
     }
 
     public function userAnswer($username)
@@ -26,12 +25,10 @@ class BackManager extends Manager
 
         $answer = $data['reponse'];
         return $answer;
-
     }
 
     public function resetPassword($username, $password)
     {
-
         $bdd = $this->dbConnect();
         $bdd->exec('UPDATE userbank SET password = "' . $password . '" WHERE username = "' . $username . '"');
     }
@@ -67,25 +64,6 @@ class BackManager extends Manager
                 $partner[] = $data;
             }
 
-
-
-        /*$cde=new PartnerCde();
-        $cde=$cde->Get_All();
-
-        $dsa=new PartnerDsa();
-        $dsa=$dsa->Get_All();
-
-        $co=new PartnerCo();
-        $co=$co->Get_All();
-
-        $pro=new PartnerPro();
-        $pro=$pro->Get_All();
-
-        $partner=[];
-        array_push($partner, $cde,$dsa,$co,$pro);
-
-        */
-
         return $partner;
     }
 
@@ -96,9 +74,7 @@ class BackManager extends Manager
 
         $partner = $reqActor->fetch();
         return $partner;
-
     }
-
 
 
      public function add_com($partner, $comment, $auteur)
@@ -110,86 +86,72 @@ class BackManager extends Manager
              'id_acteur' => $partner,
              'post'=> $comment
          ));
-
          $comment='valide';
          return $comment;
-        }
+     }
 
-        public function list_com($partner)
-        {
-            /*$bdd = $this->dbConnect();
+     public function list_com($partner)
+     {
+         $bdd = $this->dbConnect();
 
-            $req_list = $bdd->QUERY('SELECT post, id_user, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation FROM PartnerGBAF WHERE id_acteur="' . $idName . '" ORDER BY date_creation DESC');
+         $req_list = $bdd->QUERY('SELECT u.prenom AS prenom, DATE_FORMAT(p.date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation , p.post AS post FROM userbank u INNER JOIN PartnerGBAF p ON u.id_user = p.id_user WHERE p.id_acteur="' . $partner . '" ORDER BY date_creation DESC');
 
-            while ($data = $req_list->fetch()) {
-                $comments[] = $data;
-            }
+         $comments=[];
 
-            return $comments;*/
-
-            $bdd = $this->dbConnect();
-
-            $req_list = $bdd->QUERY('SELECT u.prenom AS prenom, DATE_FORMAT(p.date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation , p.post AS post FROM userbank u INNER JOIN PartnerGBAF p ON u.id_user = p.id_user WHERE p.id_acteur="' . $partner . '" ORDER BY date_creation DESC');
-
-            $comments=[];
-
-            while ($data = $req_list->fetch()) {
-                $comments[] = $data;
-            }
-            return $comments;
-
-        }
+         while ($data = $req_list->fetch()) {
+             $comments[] = $data;
+         }
+         return $comments;
+     }
 
 
-        public function list_up($partner)
-        {
-            $bdd = $this->dbConnect();
+     public function list_up($partner)
+     {
+         $bdd = $this->dbConnect();
 
-            $req_up = $bdd->QUERY('SELECT vote FROM vote WHERE vote="good" and id_acteur="' . $partner . '"');
+         $req_up = $bdd->QUERY('SELECT vote FROM vote WHERE vote="good" and id_acteur="' . $partner . '"');
 
-                while ($data = $req_up->fetch()) {
-                    $comments_up[] = $data;
-                }
+         while ($data = $req_up->fetch()) {
+             $comments_up[] = $data;
+         }
 
-                return $comments_up;
+         return $comments_up;
+     }
 
-        }
+     public function list_down($partner)
+     {
+         $bdd = $this->dbConnect();
 
-         public function list_down($partner)
-        {
-            $bdd = $this->dbConnect();
+         $req_down = $bdd->QUERY('SELECT vote FROM vote WHERE vote="bad" and id_acteur="' . $partner . '"');
 
-            $req_down = $bdd->QUERY('SELECT vote FROM vote WHERE vote="bad" and id_acteur="' . $partner . '"');
+         $comments_down=[];
 
-                $comments_down=[];
+         while ($data = $req_down->fetch()) {
+             $comments_down[] = $data;
+         }
 
-                while ($data = $req_down->fetch()) {
-                    $comments_down[] = $data;
-                }
+         return $comments_down;
+     }
 
-                return $comments_down;
-        }
+     public function add_avis($partner,$auteur ,$avis)
+     {
+         $bdd = $this->dbConnect();
+         $reqAvis = $bdd->QUERY('SELECT vote FROM vote WHERE id_user="' . $auteur . '"and id_acteur= "' . $partner . '"');
 
-        public function add_avis($partner,$auteur ,$avis){
+         if($data = $reqAvis->fetch()){
 
-            $bdd = $this->dbConnect();
-            $reqAvis = $bdd->QUERY('SELECT vote FROM vote WHERE id_user="' . $auteur . '"and id_acteur= "' . $partner . '"');
+             $message = 'vous avez déjà donné votre avis';
+             return $message;
 
-            if($data = $reqAvis->fetch()){
-
-                $message = 'vous avez déjà donné votre avis';
-                return $message;
-
-            }else{
-                $req = $bdd->prepare('INSERT INTO vote (id_user, id_acteur, vote) VALUES (:id_user, :id_acteur, :vote)');
-                $req->execute(array(
-                'id_user' => $auteur,
-                'id_acteur' => $partner,
-                'vote' => $avis
-                ));
-
-            }
-        }
+         }else{
+             $req = $bdd->prepare('INSERT INTO vote (id_user, id_acteur, vote) VALUES (:id_user, :id_acteur, :vote)');
+             $req->execute(array(
+                 'id_user' => $auteur,
+                 'id_acteur' => $partner,
+                 'vote' => $avis
+             ));
+         }
+     }
 
 
 }
