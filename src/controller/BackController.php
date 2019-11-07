ac6a9f5e137a7090ec2f3103da75210aa766bd96
Celine <celi.caurier@gmail.com>
2019-11-07 13:31:37 +0100

    functional form in page contact

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -151,5 +151,61 @@ class BackController extends PartnerController
 
     }
 
+    public function contact()
+    {
+
+        $message='* message envoyé *';
+
+        $to      = 'celi.caurier@gmail.com';
+        $subject = $_POST['demande'];
+        $corpsMail = $_POST['contact'];
+        $headers = array(
+            'From' => 'contactGBAF.fr',
+            'Reply-To' => $_POST['mail'],
+            'X-Mailer' => 'PHP/' . phpversion()
+        );
+
+       mail($to, $subject, $corpsMail, $headers);
+
+
+    /*SUR LE SERVEUR
+
+        $name       = @trim(stripslashes($_POST['nom']));
+        $fromBis    = @trim(stripslashes($_POST['mail']));
+        $from       = 'contact@gbaf.fr';
+        $subject    = @trim(stripslashes($_POST['demande']));
+        $corps      = @trim(stripslashes($_POST['contact']));
+        $to         = 'celi.caurier@gmail.com';
+
+
+        $headers  = "";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+        $headers .= "From: ContactGBAF <{$from}>\r\n";
+        $headers .= "Reply-To: <{$fromBis}>\r\n";
+        $headers .= "Subject: {$subject}\r\n";
+        $headers .= "X-Mailer: PHP/".phpversion()."\r\n";
+
+        mail($to, $subject, $corps, $headers);
+
+        return $this->twig->render('contact.twig', array(
+            'message'=> $message
+        ));
+     */
+
+
+
+
+
+
+
+
+
+
+        return $this->twig->render('contact.twig', array(
+            'message'=> $message
+        ));
+
+    }
 
 }
